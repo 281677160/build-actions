bedc129a8c152a0c7d1757025c9324faf43a1025
small1ee <95616273+small1ee@users.noreply.github.com>
2021-12-06 17:41:38 +0800

    Update diy-part.sh

--- build/Lede_minano/diy-part.sh
+++ build/Lede_minano/diy-part.sh
@@ -22,7 +22,11 @@ uci commit network                                                          # �
 uci set system.@system[0].hostname='OpenWrt-123'                            # 修改主机名称为OpenWrt-123
 EOF
 
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile            # 选择argon为默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile           # 选择argon为默认主题
+
+sed -i ‘$a src-git small-package https://github.com/small1ee/small-package' feed.conf.default     #添加small-package到源
+
+sed -i ‘$a src-git small-package https://github.com/small1ee/luci-app-overwall' feed.conf.default #添加overwall到源
 
 sed -i "s/OpenWrt /${Author} compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ           # 增加个性名字 ${Author} 默认为你的github帐号
 

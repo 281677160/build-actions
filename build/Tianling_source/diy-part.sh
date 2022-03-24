6c178ecc52051cc98d9add7d29f672f6b056bc2f
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-24 23:32:22 +0800

    Update diy-part.sh

--- build/Tianling_source/diy-part.sh
+++ build/Tianling_source/diy-part.sh
@@ -23,8 +23,6 @@ uci set system.@system[0].hostname='OpenWrt-123'                            # �
 #sed -i 's/\/bin\/login/\/bin\/login -f root/' /etc/config/ttyd              # 设置ttyd免帐号登录，如若开启，进入OPENWRT后可能要重启一次才生效（去掉uci前面的#生效）
 EOF
 
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile             # 选择argon为默认主题
-
 sed -i "s/OpenWrt /${Author} compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ_PATH       # 增加个性名字 ${Author} 默认为你的github帐号
 
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ_PATH                                                         # 设置密码为空

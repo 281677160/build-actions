54ac7692fb0df85716ef6cb9f74cddc639f2ebc0
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-27 00:13:43 +0800

    Update diy-part.sh

--- build/openwrt_amlogic/diy-part.sh
+++ build/openwrt_amlogic/diy-part.sh
@@ -32,6 +32,15 @@ sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ_PATH                                     
 sed -i '/to-ports 53/d' $ZZZ_PATH                                                                   # 删除默认防火墙
 
 
+
+# 设置打包固件的机型，内核组合（可用内核是时时变化的,过老的内核就删除的，所以要选择什么内核请看说明）
+cat >$GITHUB_WORKSPACE/amlogic_openwrt <<-EOF
+amlogic_model=s905x3_s905x2_s905x_s905w_s905d_s922x_s912
+amlogic_kernel=5.10.100_5.4.180 -a true
+rootfs_size=960
+EOF
+
+
 # 修改插件名字
 sed -i 's/"aMule设置"/"电驴下载"/g' `grep "aMule设置" -rl ./`
 sed -i 's/"网络存储"/"NAS"/g' `grep "网络存储" -rl ./`

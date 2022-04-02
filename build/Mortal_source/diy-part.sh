28549a117a14af86624e32689304b9a91758a17e
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-02 18:34:50 +0800

    Update diy-part.sh

--- build/Mortal_source/diy-part.sh
+++ build/Mortal_source/diy-part.sh
@@ -74,6 +74,7 @@ sed -i 's/"Argon 主题设置"/"Argon设置"/g' `grep "Argon 主题设置" -rl .
 
 # 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间(根据编译机型变化,自行调整删除名称)
 cat >${GITHUB_WORKSPACE}/Clear <<-EOF
+rm -rf packages
 rm -rf config.buildinfo
 rm -rf feeds.buildinfo
 rm -rf openwrt-x86-64-generic-kernel.bin

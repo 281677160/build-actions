77d87a705b6be0bf2d214fd3772cfba5325555cd
281677160 <66651997+281677160@users.noreply.github.com>
2022-05-10 21:11:30 +0800

    Update diy-part.sh

--- build/Lienol_source/diy-part.sh
+++ build/Lienol_source/diy-part.sh
@@ -43,6 +43,10 @@ sed -i "s/bootstrap/argon/ig" feeds/luci/collections/luci/Makefile
 sed -i "s/OpenWrt /${Author} compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" "$ZZZ_PATH"
 
 
+# 修改默认内核（所有机型都适用，只要您编译的机型源码附带了其他内核，请至编译说明的第12条查看）
+#sed -i 's/PATCHVER:=5.10/PATCHVER:=5.4/g' target/linux/x86/Makefile
+
+
 # 取消路由器每天跑分任务
 sed -i "/exit 0/i\sed -i '/coremark/d' /etc/crontabs/root" "$FIN_PATH"
 

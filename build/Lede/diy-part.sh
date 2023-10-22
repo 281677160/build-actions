1dec92e940f0e2f8bcca5ce5dbf03cf890cacae2
linai081795 <linai081795@gmail.com>
2023-10-22 17:13:49 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -6,7 +6,7 @@
 
 
 # 后台IP设置
-export Ipv4_ipaddr="192.168.100.200"            # 修改openwrt后台地址(填0为关闭)
+export Ipv4_ipaddr="192.168.100.2"            # 修改openwrt后台地址(填0为关闭)
 export Netmask_netm="255.255.255.0"         # IPv4 子网掩码（默认：255.255.255.0）(填0为不作修改)
 export Op_name="OpenWrt"                # 修改主机名称为OpenWrt-123(填0为不作修改)
 

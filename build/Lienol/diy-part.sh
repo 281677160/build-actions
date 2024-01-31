5e2f39531c5f5eca0fd376ff827de49f0c273ec8
无限远 <77979934+xlfq3136@users.noreply.github.com>
2024-01-31 12:37:31 +0800

    Update diy-part.sh

--- build/Lienol/diy-part.sh
+++ build/Lienol/diy-part.sh
@@ -4,6 +4,10 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
+# Add a feed source
+echo 'src-git aliyundrive-webdav https://github.com/messense/aliyundrive-webdav' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
 
 # 后台IP设置
 export Ipv4_ipaddr="0"            # 修改openwrt后台地址(填0为关闭)

6ee2ecd5a5c58f33747f82ead2c4eaecc954513f
无限远 <77979934+xlfq3136@users.noreply.github.com>
2024-02-02 22:43:17 +0800

    Update diy-part.sh

--- build/Official/diy-part.sh
+++ build/Official/diy-part.sh
@@ -6,9 +6,9 @@
 
 
 # 后台IP设置
-export Ipv4_ipaddr="0"            # 修改openwrt后台地址(填0为关闭)
-export Netmask_netm="0"           # IPv4 子网掩码（默认：255.255.255.0）(填0为不作修改)
-export Op_name="0"                # 修改主机名称为OpenWrt-123(填0为不作修改)
+export Ipv4_ipaddr="192.168.1.1"            # 修改openwrt后台地址如192.168.1.1(填0为关闭)
+export Netmask_netm="255.255.255.0"           # IPv4 子网掩码（默认：255.255.255.0）(填0为不作修改)
+export Op_name="HiWifi HC5962"                # 修改主机名称如为OpenWrt-123(填0为不作修改)
 
 # 内核和系统分区大小(不是每个机型都可用)
 export Kernel_partition_size="0"            # 内核分区大小,每个机型默认值不一样 (填写您想要的数值,默认一般16,数值以MB计算，填0为不作修改),如果你不懂就填0
@@ -38,13 +38,13 @@ export OpenClash_branch="0"                 # OpenClash的源码分别有【mast
 export OpenClash_Core="1"                   # 增加OpenClash时,把核心下载好,(填1为下载【dev单核】,填2为下载【dev/meta/premium三核】,填0为不需要核心)
 
 # 个性签名,默认增加年月日"$(TZ=UTC-8 date "+%Y.%m.%d")"
-export Customized_Information="0"  # 个性签名,你想写啥就写啥，(填0为不作修改)
+export Customized_Information="I Love You"  # 个性签名,你想写啥就写啥，(填0为不作修改)
 
 # 更换固件内核
 export Replace_Kernel="0"                    # 更换内核版本,在对应源码的[target/linux/架构]查看patches-x.x,看看x.x有啥就有啥内核了(填入内核x.x版本号,填0为不作修改)
 
 # 设置免密码登录(个别源码本身就没密码的)
-export Password_free_login="0"               # 设置首次登录后台密码为空（进入openwrt后自行修改密码）(1为启用命令,填0为不作修改)
+export Password_free_login="1"               # 设置首次登录后台密码为空（进入openwrt后自行修改密码）(1为启用命令,填0为不作修改)
 
 # 增加AdGuardHome插件和核心
 export AdGuardHome_Core="1"                  # 编译固件时自动增加AdGuardHome插件和AdGuardHome插件核心,需要注意的是一个核心20多MB的,小闪存机子搞不来(1为启用命令,填0为不作修改)

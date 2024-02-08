476dfbbda7fa872fbec8e370c1ab92fb9c1ef1df
无限远 <77979934+xlfq3136@users.noreply.github.com>
2024-02-08 22:55:57 +0800

    Update diy-part.sh

--- build/Lienol/diy-part.sh
+++ build/Lienol/diy-part.sh
@@ -49,7 +49,7 @@ export Replace_Kernel="0"                    # 更换内核版本,在对应源�
 export Password_free_login="0"               # 设置首次登录后台密码为空（进入openwrt后自行修改密码）(1为启用命令,填0为不作修改)
 
 # 增加AdGuardHome插件和核心
-export AdGuardHome_Core="1"                  # 编译固件时自动增加AdGuardHome插件和AdGuardHome插件核心,需要注意的是一个核心20多MB的,小闪存机子搞不来(1为启用命令,填0为不作修改)
+export AdGuardHome_Core="0"                  # 编译固件时自动增加AdGuardHome插件和AdGuardHome插件核心,需要注意的是一个核心20多MB的,小闪存机子搞不来(1为启用命令,填0为不作修改)
 
 # 禁用ssrplus和passwall的NaiveProxy
 export Disable_NaiveProxy="1"                # 因个别源码的分支不支持编译NaiveProxy,不小心选择了就编译错误了,为减少错误,打开这个选项后,就算选择了NaiveProxy也会把NaiveProxy干掉不进行编译的(1为启用命令,填0为不作修改)
@@ -78,16 +78,8 @@ export Cancel_running="1"                    # 取消路由器每天跑分任务
 # 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间(根据编译机型变化,自行调整删除名称)
 cat >"$CLEAR_PATH" <<-EOF
 packages
-#config.buildinfo
-#feeds.buildinfo
-#sha256sums
-#version.buildinfo
-#profiles.json
 hiwifi_hc5962-initramfs-kernel.bin
-#openwrt-x86-64-generic.manifest
-openwrt-x86-64-generic-squashfs-rootfs.img.gz
 ipk.tar.gz
-Source code
 EOF
 
 # 在线更新时，删除不想保留固件的某个文件，在EOF跟EOF之间加入删除代码，记住这里对应的是固件的文件路径，比如： rm -rf /etc/config/luci

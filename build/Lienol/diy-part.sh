9991c9fd804b9eef5b8fc4ce52ac1c274c35eb46
无限远 <77979934+xlfq3136@users.noreply.github.com>
2024-02-09 09:32:22 +0800

    试验加入拉取插件代码

--- build/Lienol/diy-part.sh
+++ build/Lienol/diy-part.sh
@@ -4,8 +4,11 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
-# 自行拉取插件
-# 此行代码失败 git clone https://github.com/messense/aliyundrive-webdav package/luci-app-aliyundrive-webdav
+# 自行拉取插件，例aliyun-webdav
+cd openwrt
+git clone https://github.com/messense/aliyundrive-webdav package/luci-app-aliyundrive-webdav
+./scripts/feeds update -a
+./scripts/feeds install -a
 
 # 后台IP设置
 export Ipv4_ipaddr="0"            # 修改openwrt后台地址(填0为关闭)

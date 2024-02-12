ce42b39de0ded2a0e1f180dcf437a0c383e6861b
无限远 <77979934+xlfq3136@users.noreply.github.com>
2024-02-12 16:23:24 +0800

    Update diy-part.sh

--- build/Lienol/diy-part.sh
+++ build/Lienol/diy-part.sh
@@ -4,7 +4,7 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
-# 自行aliyun-webdav插件
+# 自行增加拉取插件，例如aliyun-webdav插件
 git clone https://github.com/messense/aliyundrive-webdav package/luci-app-aliyundrive-webdav
 
 # 后台IP设置

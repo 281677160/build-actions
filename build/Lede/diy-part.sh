51024d74e969d9ea23cf3a87d299494830c7242b
无限远 <77979934+xlfq3136@users.noreply.github.com>
2024-02-13 13:50:28 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -91,14 +91,15 @@ sed -i 's/"设置向导"/"向导"/g' `egrep "设置向导" -rl ./`
 # 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间(根据编译机型变化,自行调整删除名称)
 cat >"$CLEAR_PATH" <<-EOF
 packages
-config.buildinfo
+#config.buildinfo
 feeds.buildinfo
-sha256sums
-version.buildinfo
-profiles.json
-openwrt-x86-64-generic-kernel.bin
-openwrt-x86-64-generic.manifest
-openwrt-x86-64-generic-squashfs-rootfs.img.gz
+#sha256sums
+#version.buildinfo
+#profiles.json
+hiwifi_hc5962-initramfs-kernel.bin
+hiwifi_hc5962-squashfs-sysupgrade.bin
+hiwifi_hc5962.manifest
+ipk.tar.gz
 EOF
 
 # 在线更新时，删除不想保留固件的某个文件，在EOF跟EOF之间加入删除代码，记住这里对应的是固件的文件路径，比如： rm -rf /etc/config/luci

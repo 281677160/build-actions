78a55522b2e0d8de91efd99a3f2c5631663038ed
ydfk <lyh6728326@gmail.com>
2024-06-01 22:25:43 +0800

    修改tailscale配置

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -4,8 +4,10 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
-sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
-git clone https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
+# sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
+# git clone https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
+
+git clone https://github.com/Carseason/openwrt-tailscale/tree/main/luci-app-tailscaler
 
 # 后台IP设置
 export Ipv4_ipaddr="10.0.0.252"            # 修改openwrt后台地址(填0为关闭)
